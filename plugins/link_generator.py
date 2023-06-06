e7fce18ed964e1c97ce136f17db9e5ec45d1452d
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-07 01:25:08 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -45,7 +45,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://telegram.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Link</b>: {link}", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"{file_name}\n\n<b>Link</b>: {link}", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
